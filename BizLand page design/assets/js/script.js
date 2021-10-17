5b683001aab1e0ce999f296882b0f19d7160e07a
MD Forhadul Islam <91374251+mdforhadulislam@users.noreply.github.com>
2021-10-17 10:19:13 -0700

    Update script.js

--- BizLand page design/assets/js/script.js
+++ BizLand page design/assets/js/script.js
@@ -1,1 +1,10 @@
-console.log("hello");
+
+    const contact_details = document.querySelector('header .header-contact-details')
+    window.addEventListener('scroll', () => {
+      const scrollyValue = window.scrollY
+      if (scrollyValue > 0) {
+        contact_details.style.marginTop = "-63px"
+      } else {
+        contact_details.style.marginTop = "0"
+      }
+    })
